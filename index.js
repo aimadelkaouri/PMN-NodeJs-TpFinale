279a6ddc6c64e655aa0d3198a87310cba03f9eec
Aimad Elkaouri <aimadelkaouri@gmail.com>
2025-03-13 23:36:49 +0100

    css

--- index.js
+++ index.js
@@ -23,23 +23,18 @@ mongoose
 
 
   app.post("/livres", async (req, res) => {
-    const newLivre = new Livre({ titre: req.body.titre, auteur: req.body.auteur, date: req.body.date, resume: req.body.resume, });
+    const newLivre = new Livre({ titre: req.body.titre, auteur: req.body.auteur, date: new Date(req.body.date), resume: req.body.resume, });
     try {
       await newLivre.save();
       res.redirect("/livres");
     } catch (error) {
-      res
-        .status(400)
-        .send("Erreur lors de l'ajout d'un livre! ," + error.message);
+      res.status(400).send("Erreur lors de l'ajout d'un livre! ," + error.message);
     }
   });
 
 
   app.get("/livres", async (req, res) => {
     try {
-    //     const livres = await Livre.find();
-    //   res.render("view_livre", {livres});
-
     let page = parseInt(req.query.page) || 1; // Page actuelle, par défaut 1
         let limit = 2; // Nombre de livres par page
         let skip = (page - 1) * limit; // Calcul de l'offset
@@ -52,8 +47,6 @@ mongoose
             currentPage: page, 
             totalPages: Math.ceil(totalLivres / limit) 
         });
-
-
     } catch (error) {
       res.status(500).send("Erreur lors de la récupération des livres");
     }
@@ -62,6 +55,11 @@ mongoose
   app.get("/livres/edit/:id", async (req, res) => {
     try {
         const livre = await Livre.findById(req.params.id);
+  if (livre.date instanceof Date) {
+    livre.date = livre.date.toISOString().split("T")[0]; // ✅ Convertir en YYYY-MM-DD
+}
+
+
       res.render("edit_livre", {livre});
     } catch (error) {
       console.log(error)
@@ -75,9 +73,7 @@ app.post("/livres/:id", async (req, res) => {
       await Livre.findByIdAndDelete(req.params.id);
       res.redirect("/livres");
     } catch (error) {
-      res
-        .status(500)
-        .send("Erreur lors de la suppression du livre: " + error.message);
+      res.status(500).send("Erreur lors de la suppression du livre: " + error.message);
     }
   });
   
--- models/livre.js
+++ models/livre.js
@@ -4,7 +4,7 @@ const mongoose = require ('mongoose')
 const livreSchema  = new mongoose.Schema({
     titre: { type:String, required:true,  minlength: 3},
     auteur: { type:String, required:true,  minlength: 3},
-    date: { type:Number, required:true},
+    date: { type:Date, required:true},
     resume: { type:String, required:false,  maxlength: 500}
 })
 
